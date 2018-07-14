55dbfb2770ecb856a91cce21a799771c4c1a9e17
Jessica Rowland <rowlandjl@vcu.edu>
2018-07-14 19:42:41 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -35,6 +35,7 @@ def turn(board)
   input = gets.strip
   index = input_to_index(input)
 
-  if valid_move?(board, index) == true 
+  if valid_move?(board, index) == true
     move(board, index)
+    display_board(board)
 end
